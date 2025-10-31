0bd6d0a13edea98fd03e906a1ea91ebccbf5a5d7
jisvarun <jisbarunpandit@gmail.com>
2025-10-31 23:17:02 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MyButton from './components/MyButton'
 import './App.css'
 
+const user = {
+  name: 'Varun',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90
+};
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{user.name}</h1>
+      <img 
+        src={user.imageUrl}
+        alt={'photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+          borderRadius: '50%'
+        }}
+        />
+      <MyButton />
+      <ul>{listItems}</ul>
     </>
   )
 }
